b5182ba5dac6aaf263070a9df547fa7ce4de1baa
Cristian Álvarez <94159947+CristianAlvarezMartinez@users.noreply.github.com>
2023-04-21 14:16:46 -0500

    b-04 (#4)

---
+++ models/Company.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+let schema = new mongoose.Schema({
+    name: {type: String, required: true},
+    logo: {type: String, required: true},
+    website: {type: String, required: true},
+    description: {type: String, required: false},
+    active: {type: Boolean, required: true},
+    user_id: {type: mongoose.Types.ObjectId, href: 'users', required: true},
+},{
+    timestamps: true
+})
+
+let collection = 'companies'
+
+let company = mongoose.model(schema, collection)
+export default company
