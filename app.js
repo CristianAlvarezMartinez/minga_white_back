5a72d9758cec11b8c238d8ea7a46e8d97db0c4ce
CrisDev <alvarezmartinez132@gmail.com>
2023-04-24 11:18:18 -0500

    m-02

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import './config/database.js'
 import createError from 'http-errors';
 import express from 'express';
 
+import cors from 'cors'
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan'
@@ -19,6 +20,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
